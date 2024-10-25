3b419544a791710a492be2a88d9f3afb80350f7b
sghbrookes <92637876+sghbrookes@users.noreply.github.com>
2024-10-25 10:12:49 +0100

    Update plot_fes.py

--- resources/metadynamics/plot_fes.py
+++ resources/metadynamics/plot_fes.py
@@ -7,7 +7,7 @@ from scipy.interpolate import griddata
 fin = np.loadtxt('./fes.dat')
 x = np.degrees(fin[:, 0])
 y = np.degrees(fin[:, 1])
-z = fin[:, 2]
+z = fin[:, 2] * 23.0609
 x[x < -90] += 360
 y[y < -90] += 360
 xnew, ynew = np.meshgrid(np.linspace(-90, 270, num=len(np.unique(x))),
@@ -16,9 +16,9 @@ znew = griddata((x, y), z, (xnew, ynew), method='linear')
 znew -= np.nanmin(znew)
 
 # plot
-max_val = 2
+max_val = 10 # change to alter appearance of figure
 fig, ax = plt.subplots()
-levels = np.round(np.linspace(0, max_val, 10*int((max_val))),1)
+levels = np.round(np.linspace(0, max_val, 2*int((max_val))),1)
 cs = ax.contourf(xnew,ynew,znew,
                   vmin=0,vmax=max_val,
                   levels=levels,
@@ -31,3 +31,4 @@ ax.set_ylabel(r'$\theta$  (degrees)',size=12)
 
 plt.savefig('./conf_free_energy.png',dpi=400,bbox_inches='tight',transparent=False, edgecolor='none')
 plt.show()
+
